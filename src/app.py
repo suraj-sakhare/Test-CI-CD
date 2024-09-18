2911f50f036a3d6a70dcc533929a3493d5e9ac59
suraj-sakhare <suraj.sakhare@payppy.co>
2024-09-18 16:45:42 +0530

    addition

--- src/app.py
+++ src/app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 # This is a Hello World Code!!
 @app.route('/')
 def hello_world():
-    return 'Hello, Payppy! :) Jay Ganesh! '
+    return 'Hello, Payppy! :)'
+
+@app.route('/Addtion')
+def Addition(num:a, num:b)->num:
+    a = 4
+    b = 5
+    return  a+b   
 
 
 
